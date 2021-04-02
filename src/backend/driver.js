d0beaf9edb98c0d5c2f8ebe17f0563bcc81202cb
Jan Prochazka <jan.prochazka@gmail.com>
2021-04-02 12:29:27 +0200

    initial import - works connect, get version, get list of databases

---
+++ src/backend/driver.js
@@ -0,0 +1,56 @@
+const _ = require('lodash');
+const stream = require('stream');
+const driverBase = require('../frontend/driver');
+const Analyser = require('./Analyser');
+const MongoClient = require('mongodb').MongoClient;
+
+/** @type {import('dbgate-types').EngineDriver} */
+const driver = {
+  ...driverBase,
+  analyserClass: Analyser,
+  async connect({ server, port, user, password, database }) {
+    let mongoUrl = user ? `mongodb://${user}:${password}@${server}:${port}` : `mongodb://${server}:${port}`;
+    if (database) mongoUrl += '/' + database;
+
+    const pool = new MongoClient(mongoUrl);
+    await pool.connect();
+    // const pool = await MongoClient.connect(mongoUrl);
+    return pool;
+  },
+  // @ts-ignore
+  async query(pool, sql) {
+    return {
+      rows: [],
+      columns: [],
+    };
+  },
+  async stream(pool, sql, options) {
+    return null;
+  },
+  async readQuery(pool, sql, structure) {
+    const pass = new stream.PassThrough({
+      objectMode: true,
+      highWaterMark: 100,
+    });
+
+    // pass.write(structure)
+    // pass.write(row1)
+    // pass.write(row2)
+    // pass.end()
+
+    return pass;
+  },
+  async writeTable(pool, name, options) {
+    return createBulkInsertStreamBase(this, stream, pool, name, options);
+  },
+  async getVersion(pool) {
+    const status = await pool.db().admin().serverInfo();
+    return status;
+  },
+  async listDatabases(pool) {
+    const res = await pool.db().admin().listDatabases();
+    return res.databases;
+  },
+};
+
+module.exports = driver;
---
+++ src/frontend/driver.js
@@ -0,0 +1,27 @@
+const { driverBase } = require('dbgate-tools');
+const Dumper = require('./Dumper');
+
+/** @type {import('dbgate-types').SqlDialect} */
+const dialect = {
+  limitSelect: true,
+  rangeSelect: true,
+  offsetFetchRangeSyntax: true,
+  stringEscapeChar: "'",
+  fallbackDataType: 'nvarchar(max)',
+  quoteIdentifier(s) {
+    return `[${s}]`;
+  },
+};
+
+
+/** @type {import('dbgate-types').EngineDriver} */
+const driver = {
+  ...driverBase,
+  dumperClass: Dumper,
+  dialect,
+  engine: 'mongo@dbgate-plugin-mongo',
+  title: 'MongoDB',
+  defaultPort: 27017,
+};
+
+module.exports = driver;
